8ba9f5d7b4fee0721fc1aaa49219b9e0361962f5
Kevin <kvn.philippe@gmail.com>
2020-06-20 17:34:50 +0200

    remove unecessary css import

--- client/src/features/auth/components/Login.js
+++ client/src/features/auth/components/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
